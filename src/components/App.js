a55058b93ca76a18863d78f037ef867ffc535808
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-04-16 19:51:07 +0300

    made changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+// create your App component 
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+
+function App() {
+    const [imageURL,setImageURL]=useState(null)
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((resp)=>resp.json())
+        .then((data)=>{
+            setImageURL(data.message);
+
+            console.log(data.message)
+        })
+    })
+  return (
+    <div>
+      {imageURL ? <img src={imageURL} alt="A Random Dog"/> : <p>Loading...</p>}
+    </div>
+  )
+}
+
+export default App
